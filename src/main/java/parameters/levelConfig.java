b048652831afba52688c0e7f3ce08c4b978e35d3
hassansenoussi <94441244+hassansenoussi@users.noreply.github.com>
2021-12-14 11:15:46 +0100

    hassan_commit

--- src/main/java/parameters/levelConfig.java
+++ src/main/java/parameters/levelConfig.java
@@ -1,5 +1,23 @@
 package parameters;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class levelConfig {
-	
+    try{
+        InputStream flux=new FileInputStream();
+        InputStreamReader lecture=new InputStreamReader(flux);
+        BufferedReader buff=new BufferedReader(lecture);
+        String ligne;
+        while ((ligne=buff.readLine())!=null){
+            System.out.println(ligne);
+        }
+        buff.close();
+
+catch (Exception e){
+        System.out.println(e.toString());
+    }
+
 }
