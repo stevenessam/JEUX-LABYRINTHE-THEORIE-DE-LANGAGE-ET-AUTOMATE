0532446709966e306e0c21166901ba08112f5113
hassansenoussi <94441244+hassansenoussi@users.noreply.github.com>
2021-12-14 12:02:51 +0100

    lire_ficher

--- src/main/java/parameters/levelConfig.java
+++ src/main/java/parameters/levelConfig.java
@@ -5,6 +5,11 @@ import gameObject.gameScene;
 import gameObject.player;
 import ressourceManages.Textures;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class levelConfig {
 	private optionConfig option;
 	public levelConfig(optionConfig option) {
@@ -19,4 +24,28 @@ public class levelConfig {
 		scene.add(a);
 		return scene;
 	}
+
+	public String ReadeFile(String file) {
+
+		InputStream config = optionConfig.class.getResourceAsStream(file);
+		if (config != null) {
+			StringBuilder sb = new StringBuilder();
+			BufferedReader br = new BufferedReader(new InputStreamReader(config));
+			String line;
+			try {
+				while ((line = br.readLine()) != null) {
+					sb.append(line + System.lineSeparator());
+				}
+				return sb.toString().trim().replaceAll("[\n\t\r]", "");
+
+			} catch (IOException e) {
+				System.err.println("path Not find");
+			}
+
+		}else{
+			System.err.println(file + "not existe");
+		}
+		return null;
+	}
 }
+
