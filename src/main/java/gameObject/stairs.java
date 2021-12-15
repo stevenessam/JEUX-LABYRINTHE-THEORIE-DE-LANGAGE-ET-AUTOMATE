6d30b4b027cea0e04606014e5b44cc38bd0692d8
Steven Essam <stevenessams7@gmail.com>
2021-12-15 09:40:00 +0100

    Fix Stairs

--- src/main/java/gameObject/stairs.java
+++ src/main/java/gameObject/stairs.java
@@ -8,7 +8,7 @@ public class stairs extends gameObject {
     public stairs(int x, int y,stairs_item itemStair) {
         super(x, y);
         this.itemStair=itemStair;
-        setSprite(Textures.stairs, 1, 1);
+
 
     }
 
@@ -16,6 +16,7 @@ public class stairs extends gameObject {
     protected void update() {
         if(this.itemStair.isPickup()){
             this.setFrame(1+((this.getTimer()/50)+1)%2);
+            setSprite(Textures.stairs, 1, 1);
             setType(gameObjectType.PATH);
         }else{
             this.setFrame(0);
