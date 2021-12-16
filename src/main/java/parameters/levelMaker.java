107a72c8383b86638aa0e9816e55424b5fdc717d
Moha-Dan <73938112+Moha-Dan@users.noreply.github.com>
2021-12-16 11:34:19 +0100

    Update levelMaker.java

--- src/main/java/parameters/levelMaker.java
+++ src/main/java/parameters/levelMaker.java
@@ -15,42 +15,45 @@ public class levelMaker {
 	
 	public levelMaker() {
 		level.setDebug(true);
-		List<Token> group = new ArrayList<Token>();
-
+		
 		/** commentaire "--[a-z]*" */
+		List<Token> group = new ArrayList<Token>();
 		Token commentStart = new Token("--","commentStart");
-		commentStart.add(0);
 		level.addToken(commentStart);
+		commentStart.add(0);
 
-		// Token commentComment = new Token(Pattern.compile("[a-z]"),"commentComment");
-		// commentComment.add(commentStart);
-		// commentComment.add(-1);
-		// level.addToken(commentComment);
+		Token commentComment = new Token(Pattern.compile("[^\n]"),"commentComment");
+		level.addToken(commentComment);
+		commentComment.add(commentStart);
+		commentComment.add(commentComment);
 
-		// group.add(commentComment);
+		group.add(commentComment);
 		groups.add(group);
 
 		/** define block "set . to [a-z]*;" */
-
+		group = new ArrayList<Token>();
 		Token blockStart = new Token("set ","blockStart");
-		blockStart.add(0);
 		level.addToken(blockStart);
+		blockStart.add(0);
 
 		Token blockVar = new Token(Pattern.compile("."),"blockVar");
-		blockVar.add(blockStart);
 		level.addToken(blockVar);
+		blockVar.add(blockStart);
 
 		Token blockAssign = new Token(" to ","blockAssign");
-		blockAssign.add(blockVar);
 		level.addToken(blockAssign);
+		blockAssign.add(blockVar);
 
 		Token blockName = new Token(Pattern.compile("[a-z]"),"blockName");
-		blockName.add(blockAssign);
-		blockName.add(-1);
 		level.addToken(blockName);
+		blockName.add(blockAssign);
+		blockName.add(blockName);
 
-		// parser("--set vars");
-		parser("set _ to floor;");
+		group.add(blockName);
+		groups.add(group);
+
+		parser("--set vars");
+		// parser("set _ to floor;");
 	}
 	public void parser(String map){
 		List<RegonizeToken> tokens = level.regroup(new AutomateString(map),groups);
