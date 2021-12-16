09214214c9faa8941c5b415a8ed7e30b75b8552d
Moha-Dan <73938112+Moha-Dan@users.noreply.github.com>
2021-12-16 11:23:15 +0100

    Update levelMaker.java

--- src/main/java/parameters/levelMaker.java
+++ src/main/java/parameters/levelMaker.java
@@ -14,21 +14,43 @@ public class levelMaker {
 	List<List<Token>> groups = new ArrayList<List<Token>>();
 	
 	public levelMaker() {
-
+		level.setDebug(true);
 		List<Token> group = new ArrayList<Token>();
 
+		/** commentaire "--[a-z]*" */
 		Token commentStart = new Token("--","commentStart");
 		commentStart.add(0);
 		level.addToken(commentStart);
 
-		Token commentComment = new Token(Pattern.compile("[a-z]"),"commentComment");
-		commentStart.add(-1);
-		level.addToken(commentStart);
+		// Token commentComment = new Token(Pattern.compile("[a-z]"),"commentComment");
+		// commentComment.add(commentStart);
+		// commentComment.add(-1);
+		// level.addToken(commentComment);
 
-		group.add(commentComment);
+		// group.add(commentComment);
 		groups.add(group);
 
-		parser("--set vars");
+		/** define block "set . to [a-z]*;" */
+
+		Token blockStart = new Token("set ","blockStart");
+		blockStart.add(0);
+		level.addToken(blockStart);
+
+		Token blockVar = new Token(Pattern.compile("."),"blockVar");
+		blockVar.add(blockStart);
+		level.addToken(blockVar);
+
+		Token blockAssign = new Token(" to ","blockAssign");
+		blockAssign.add(blockVar);
+		level.addToken(blockAssign);
+
+		Token blockName = new Token(Pattern.compile("[a-z]"),"blockName");
+		blockName.add(blockAssign);
+		blockName.add(-1);
+		level.addToken(blockName);
+
+		// parser("--set vars");
+		parser("set _ to floor;");
 	}
 	public void parser(String map){
 		List<RegonizeToken> tokens = level.regroup(new AutomateString(map),groups);
