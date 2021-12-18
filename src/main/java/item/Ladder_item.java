f0e55a7cd6d31f1f42d84054c245d406b1be61dd
Steven Essam <stevenessams7@gmail.com>
2021-12-18 16:40:15 +0100

    commit

--- src/main/java/item/Ladder_item.java
+++ src/main/java/item/Ladder_item.java
@@ -8,6 +8,10 @@ import assets.Textures;
 import gameObject.gameObject;
 import gameObject.gameObjectType;
 
+/**
+ * Class Ladder_item qui extends la class gameObject
+ */
+
 public class Ladder_item extends gameObject{
 
 
@@ -16,6 +20,11 @@ public class Ladder_item extends gameObject{
         return pickup;
     }
 
+    /**
+     * Ladder_item est le Constructeur  qui a comme parametre x et y
+     * @param x
+     * @param y
+     */
     public Ladder_item(int x,int y) {
         super(x+.5,y+.5,Textures.stairs_item);
         setSprite(Textures.stairs_item, 1, 1);
@@ -24,18 +33,30 @@ public class Ladder_item extends gameObject{
 
     }
 
-
+    /**
+     * Methode render a comme parametre graphic qui est un variable de la class {@link GraphicsContext}
+     * @param graphic
+     */
     @Override
     public void render(GraphicsContext graphic) {
         if(!pickup)
             super.render(graphic);
     }
+
+    /**
+     * La méthode isplayerTouching permet de verifier si le player a toucher le Ladder_item
+     * @return
+     */
     private boolean isplayerTouching(){
         List<gameObject> objects = this.getScene().getObjects(getX(),getY());
         return objects.stream().filter((gameObject gO)->{
             return gameObjectType.PLAYER.equals(gO.getType());
         }).findFirst().isPresent();
     }
+
+    /**
+     * Methode update permet de metre a jour le Ladder_item quand c'est toucher par le player
+     */
     @Override
     protected void update() {
         if(!pickup){
