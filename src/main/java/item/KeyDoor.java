65a158900af52f0f9273dda70fe1ac76a111ff01
hassansenoussi <94441244+hassansenoussi@users.noreply.github.com>
2021-12-18 16:45:50 +0100

    commentaires commit

--- src/main/java/item/KeyDoor.java
+++ src/main/java/item/KeyDoor.java
@@ -7,6 +7,9 @@ import gameObject.gameObject;
 import gameObject.gameObjectType;
 import javafx.scene.canvas.GraphicsContext;
 
+/**
+ * La class KeyDoor extends gameObject
+ */
 public class KeyDoor extends gameObject{
 
 	boolean pickup = false;
@@ -14,23 +17,39 @@ public class KeyDoor extends gameObject{
 		return pickup;
 	}
 
+	/**
+	 * Constructeur de la class KeyDoor prend comme parametre x et y
+	 * @param x
+	 * @param y
+	 */
 	public KeyDoor(int x, int y) {
 		super(x+.5,y+.5,Textures.KeyDoor);
 		this.setDeltaX(.5);
 		this.setDeltaY(.5);
 	}
 
+	/**
+	 * Méthode render a comme paramétre graphic qui est un variable de la class {@link GraphicsContext}
+	 * @param graphic
+	 */
 	@Override
 	public void render(GraphicsContext graphic) {
 		if(!pickup)
 			super.render(graphic);
 	}
+	/**
+	 * test les elments avec les memes position que la potion et filtre ceux qui sont aussi joueur
+	 * @return si un joueur touche la potion
+	 */
 	private boolean isplayerTouching(){
 		List<gameObject> objects = this.getScene().getObjects(getX(),getY());
 		return objects.stream().filter((gameObject gO)->{
 			return gameObjectType.PLAYER.equals(gO.getType());
 		}).findFirst().isPresent();
 	}
+	/**
+	 * Methode update permet de metre a jour le KeyDoor quand c'est toucher par le player
+	 */
 	@Override
 	protected void update() {
 		if(!pickup){
